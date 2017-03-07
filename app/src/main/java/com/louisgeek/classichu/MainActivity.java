0f1dfcb63c8885e67e9af05464deee5e37ede419
classichu <classichu@qq.com>
2017-03-07 19:56:52 +0800

    update

--- app/src/main/java/com/louisgeek/classichu/MainActivity.java
+++ app/src/main/java/com/louisgeek/classichu/MainActivity.java
@@ -1,6 +1,11 @@
 package com.louisgeek.classichu;
 
+import android.view.Menu;
+import android.view.View;
+
+import com.classichu.classichu.basic.tool.ToastTool;
 import com.classichu.classichu.classic.ClassicActivity;
+import com.classichu.titlebar.widget.ClassicTitleBar;
 
 public class MainActivity extends ClassicActivity {
 
@@ -11,7 +16,21 @@ public class MainActivity extends ClassicActivity {
 
     @Override
     protected void initView() {
-        setAppBarTitle("dsada");
+
+        mClassicTitleBar.setCenterText("首页").setLeftText("1111").setRightText("2222")
+                //TODO ImageOrVectorResHelper
+                 .setOnTitleBarRightItemClickListener(new ClassicTitleBar.OnTitleBarRightItemClickListener() {
+                    @Override
+                    public void onRightClick(View view) {
+                        ToastTool.showImageOk("1111");
+                    }
+                });
+        mClassicTitleBar.setOnTitleBarLeftItemClickListener(new ClassicTitleBar.OnTitleBarLeftItemClickListener() {
+            @Override
+            public void onLeftClick(View view) {
+                ToastTool.showImageOk("22222");
+            }
+        });
     }
 
     @Override
@@ -26,11 +45,17 @@ public class MainActivity extends ClassicActivity {
 
     @Override
     protected AppBarStyle configAppBarStyle() {
-        return AppBarStyle.ClassicTitleBar;
+        return AppBarStyle.ClassicTitleBarInFrameLayout;
     }
 
     @Override
     protected void initListener() {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_simple, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
